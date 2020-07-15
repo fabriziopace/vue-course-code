5d54e4fcf99d103fea0700fd5b223ac4e994eb2b
unknown <pacefabriziowork@gmail.com>
2020-07-15 22:51:10 +0200

    Using Custom Events for Child>Parent Communication

--- projects/communicating-betw-components/src/components/UserDetail.vue
+++ projects/communicating-betw-components/src/components/UserDetail.vue
@@ -3,15 +3,25 @@
         <h3>You may view the User Details here</h3>
         <p>Many Details</p>
         <p>User Name: {{ switchName() }}</p>
+        <button @click="resetName">Reset Name</button>
     </div>
 </template>
 
 <script>
     export default {
-        props: ['myName'],
+        props: {
+            myName: {
+                type: String,
+                default: 'Max'
+            }
+        },
         methods: {
             switchName() {
                 return this.myName.split("").reverse().join("");
+            },
+            resetName() {
+                this.myName = 'Max';
+                this.$emit('nameWasReset', this.myName);
             }
         }
     }
