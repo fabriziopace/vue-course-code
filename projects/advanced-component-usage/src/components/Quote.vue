b8c55717e6615d672692eeeba9546603eba7b1f8
unknown <pacefabriziowork@gmail.com>
2020-07-23 22:22:35 +0200

    Switching Multiple Comp. with Dynamic Components

--- projects/advanced-component-usage/src/components/Quote.vue
+++ projects/advanced-component-usage/src/components/Quote.vue
@@ -2,17 +2,20 @@
   <div>
     <div class="title">
       <slot name="title"></slot>
+      <span style="color: #ccc;">
+        <slot name="subtitle">The Subtitle</slot>
+      </span>
     </div>
     <hr />
     <div>
-      <slot name="content"></slot>
+      <slot></slot>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  props: ["quote"]
+  props: ["quote"],
 };
 </script>
 
