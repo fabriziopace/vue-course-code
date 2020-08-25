0de95779469b089c02996d2889ff2bcac3f3d56b
Fabrizio Pace <pacefabriziowork@gmail.com>
2020-08-25 20:57:04 +0200

    Creating slide and fade transition with CSS

--- projects/adding-animations-trans/src/App.vue
+++ projects/adding-animations-trans/src/App.vue
@@ -7,7 +7,10 @@
         <button class="btn btn-primary" @click="show =  !show">Show Alert</button>
         <br />
         <br />
-        <transition>
+        <transition name="fade">
+          <div class="alert alert-info" v-if="show">This is some Info</div>
+        </transition>
+        <transition name="slide">
           <div class="alert alert-info" v-if="show">This is some Info</div>
         </transition>
       </div>
@@ -26,4 +29,46 @@ export default {
 </script>
 
 <style>
+.fade-enter {
+  opacity: 0;
+}
+.fade-enter-active {
+  transition: opacity 1s;
+}
+.fade-leave {
+  /* opacity: 1; */
+}
+.fade-leave-active {
+  transition: opacity 1s;
+  opacity: 0;
+}
+.slide-enter {
+  /* transform: translateY(20px); */
+}
+.slide-enter-active {
+  animation: slide-in 1s ease-out forwards;
+}
+.slide-leave {
+}
+.slide-leave-enter {
+  animation: slide-out 1s ease-out forwards;
+}
+
+@keyframes slide-in {
+  from {
+    transform: translateY(20px);
+  }
+  to {
+    transform: translateY(0);
+  }
+}
+
+@keyframes slide-out {
+  from {
+    transform: translateY(0);
+  }
+  to {
+    transform: translateY(20px);
+  }
+}
 </style>
