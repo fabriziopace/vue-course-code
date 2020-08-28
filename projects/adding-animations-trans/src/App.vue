417f444aee5cef00b1dfdd034f921a3c9ed62fad
Fabrizio Pace <pacefabriziowork@gmail.com>
2020-08-28 21:39:58 +0200

    Using Dynamic Names and Attributes

--- projects/adding-animations-trans/src/App.vue
+++ projects/adding-animations-trans/src/App.vue
@@ -4,16 +4,24 @@
       <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
         <h1>Animations</h1>
         <hr />
+        <select v-model="alertAnimation" class="form-control">
+          <option value="fade">Fade</option>
+          <option value="slide">Slide</option>
+        </select>
+        <br />
         <button class="btn btn-primary" @click="show =  !show">Show Alert</button>
         <br />
         <br />
-        <transition name="fade">
+        <transition :name="alertAnimation">
           <div class="alert alert-info" v-show="show">This is some Info</div>
         </transition>
         <transition name="slide" type="animation">
           <div class="alert alert-info" v-if="show">This is some Info</div>
         </transition>
-        <transition enter-active-class="animate__animated animate__bounce" leave-active-class="animate__animated animate__shakeX">
+        <transition
+          enter-active-class="animate__animated animate__bounce"
+          leave-active-class="animate__animated animate__shakeX"
+        >
           <div class="alert alert-info" v-if="show">This is some Info</div>
         </transition>
       </div>
@@ -26,6 +34,7 @@ export default {
   data() {
     return {
       show: true,
+      alertAnimation: "fade",
     };
   },
 };
