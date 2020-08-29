ba0ec55d6cfcf8979bb5d1b848870d7babffdfba
Fabrizio Pace <pacefabriziowork@gmail.com>
2020-08-29 20:24:30 +0200

    Transitioning between Multiple Elements

--- projects/adding-animations-trans/src/App.vue
+++ projects/adding-animations-trans/src/App.vue
@@ -24,6 +24,10 @@
         >
           <div class="alert alert-info" v-if="show">This is some Info</div>
         </transition>
+        <transition :name="alertAnimation" mode="out-in">
+          <div class="alert alert-info" v-if="show" key="info">This is some Info</div>
+          <div class="alert alert-warning" v-else key="warning">This is some Warning</div>
+        </transition>
       </div>
     </div>
   </div>
@@ -66,7 +70,7 @@ export default {
 }
 .slide-leave-enter {
   animation: slide-out 1s ease-out forwards;
-  transition: opacity 3s;
+  transition: opacity 1s;
   opacity: 0;
 }
 
