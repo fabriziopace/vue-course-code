b3ac42f1951ee16f1eb3b249ff005d57000db4b3
Fabrizio Pace <pacefabriziowork@gmail.com>
2020-09-03 21:03:39 +0200

    Using <transition-group> to Animate a List

--- projects/adding-animations-trans/src/App.vue
+++ projects/adding-animations-trans/src/App.vue
@@ -59,12 +59,15 @@
         <br />
         <br />
         <ul class="list-group">
-          <li
-            class="list-group-item"
-            v-for="(number, index) in numbers"
-            @click="removeItem(index)"
-            style="cursor: pointer"
-          >{{ number }}</li>
+          <transition-group name="slide">
+            <li
+              class="list-group-item"
+              v-for="(number, index) in numbers"
+              @click="removeItem(index)"
+              style="cursor: pointer"
+              :key="number"
+            >{{ number }}</li>
+          </transition-group>
         </ul>
       </div>
     </div>
@@ -171,10 +174,15 @@ export default {
 }
 .slide-leave {
 }
-.slide-leave-enter {
+.slide-leave-active {
   animation: slide-out 1s ease-out forwards;
   transition: opacity 1s;
   opacity: 0;
+  position: absolute;
+}
+
+.slide-move {
+  transition: transform 1s;
 }
 
 @keyframes slide-in {
