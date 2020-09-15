469cf5f95fd2fec127adbb0fb83911145d708093
Fabrizio Pace <pacefabriziowork@gmail.com>
2020-09-15 21:29:29 +0200

    POSTing Data to a Server (Sending a POST Request)

--- projects/connecting-to-server-http/src/App.vue
+++ projects/connecting-to-server-http/src/App.vue
@@ -29,7 +29,19 @@ export default {
   },
   methods: {
     submit() {
-      console.log(this.user);
+      this.$http
+        .post(
+          "https://connecting-server-vue.firebaseio.com/data.json",
+          this.user
+        )
+        .then(
+          (response) => {
+            console.log(response);
+          },
+          (error) => {
+            console.log(error);
+          }
+        );
     },
   },
 };
