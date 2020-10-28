c810f86585cb18f96a81859226f2c29a16d233bf
Fabrizio Pace <pacefabriziowork@gmail.com>
2020-10-28 21:22:23 +0100

    Connecting the Portfolio with Vuex

--- projects/stock-trader-application/src/components/stocks/Stock.vue
+++ projects/stock-trader-application/src/components/stocks/Stock.vue
@@ -20,7 +20,7 @@
           <button
             class="btn btn-success"
             @click="buyStock"
-            :disabled="quantity <= 0 || !Number.isInteger(quantity)"
+            :disabled="quantity <= 0 || Number.isInteger(quantity)"
           >
             Buy
           </button>
@@ -46,6 +46,7 @@ export default {
         quantity: this.quantity,
       };
       console.log(order);
+      this.$store.dispatch("buyStock", order);
       this.quantity = 0;
     },
   },
