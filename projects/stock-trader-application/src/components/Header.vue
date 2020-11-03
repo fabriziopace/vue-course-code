e87bde634bc5f612786faf23a809feb2840f2db1
Fabrizio Pace <pacefabriziowork@gmail.com>
2020-11-03 22:05:43 +0100

    Saving Data (PUT Request)

--- projects/stock-trader-application/src/components/Header.vue
+++ projects/stock-trader-application/src/components/Header.vue
@@ -19,7 +19,11 @@
         >
         <ul class="nav navbar-nav navbar-right">
           <li><a href="#" @click="endDay">End Day</a></li>
-          <li class="dropdown" :class="{open: isDropdownOpen}" @click="isDropdownOpen = !isDropdownOpen">
+          <li
+            class="dropdown"
+            :class="{ open: isDropdownOpen }"
+            @click="isDropdownOpen = !isDropdownOpen"
+          >
             <a
               href="#"
               class="dropdown-toggle"
@@ -30,7 +34,7 @@
               >Save & Load <span class="caret"></span
             ></a>
             <ul class="dropdown-menu">
-              <li><a href="#">Save Data</a></li>
+              <li><a href="#" @click="saveData">Save Data</a></li>
               <li><a href="#">Load Data</a></li>
             </ul>
           </li>
@@ -60,6 +64,14 @@ export default {
     ...mapActions(["randomizeStocks"]),
     endDay() {
       this.randomizeStocks();
+    },
+    saveData() {
+      const data = {
+        funds: this.$store.getters.funds,
+        stockPortfolio: this.$store.getters.stockPortfolio,
+        stocks: this.$store.getters.stocks,
+      };
+      this.$http.put('data.json', data);
     },
   },
 };
