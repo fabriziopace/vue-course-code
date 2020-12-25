9ab1a7ef7464df80466fc02110ae8f4953cac0f3
Fabrizio Pace <pacefabriziowork@gmail.com>
2020-12-25 17:12:32 +0100

    Updating the Health Bars

--- projects/prj-monster-01-starting-setup/app.js
+++ projects/prj-monster-01-starting-setup/app.js
@@ -9,6 +9,18 @@ const app = Vue.createApp({
             monsterHealth: 100
         };
     },
+    computed: {
+        monsterBarStyles() {
+            return {
+                width: this.monsterHealth + '%'
+            };
+        },
+        playerBarStyles() {
+            return {
+                width: this.playerHealth + '%'
+            };
+        }
+    },
     methods: {
         attackMonster() {
             const attackValue = getRandomValue(5, 12);
