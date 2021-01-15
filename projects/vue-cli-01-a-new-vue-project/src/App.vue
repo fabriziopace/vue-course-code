66f610e1e13636cbf18a632383cc8a5c7d7117f5
Fabrizio Pace <pacefabriziowork@gmail.com>
2021-01-15 23:19:01 +0100

    Creating a Basic Vue App

---
+++ projects/vue-cli-01-a-new-vue-project/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <section>
+    <h2>My Friends</h2>
+    <ul>
+      <li v-for="friend in friends" :key="friend.id">{{friend.name}}</li>
+    </ul>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      friends: [
+        {
+          id: "manuel",
+          name: "Manuel Lorenz",
+          phone: "0123 34566 70",
+          email: "manuel@localhost.com",
+        },
+        {
+          id: "julie",
+          name: "Julie Jones",
+          phone: "3244 54221 46",
+          email: "julie@localhost.com",
+        },
+      ],
+    };
+  },
+};
+</script>
