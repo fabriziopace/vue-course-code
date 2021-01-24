79482bb265836abd77b6ccd9d6d544116e396781
Fabrizio Pace <pacefabriziowork@gmail.com>
2021-01-24 17:39:53 +0100

    Emitting Custom Event (Child-Parent Communication)

--- projects/vue-cli-01-a-new-vue-project/src/App.vue
+++ projects/vue-cli-01-a-new-vue-project/src/App.vue
@@ -5,10 +5,12 @@
       <friend-contact
         v-for="friend in friends"
         :key="friend.id"
+        :id="friend.id"
         :name="friend.name"
         :phone-number="friend.phone"
         :email-address="friend.email"
-        :is-favorite="true"
+        :is-favorite="friend.isFavorite"
+        @toggle-favorite="toggleFavoriteStatus"
       ></friend-contact>
     </ul>
   </section>
@@ -26,15 +28,23 @@ export default {
           name: "Manuel Lorenz",
           phone: "0123 34566 70",
           email: "manuel@localhost.com",
+          isFavorite: true,
         },
         {
           id: "julie",
           name: "Julie Jones",
           phone: "3244 54221 46",
           email: "julie@localhost.com",
+          isFavorite: false,
         },
       ],
     };
+  },
+  methods: {
+    toggleFavoriteStatus(friendId) {
+      const identifiedFriend = this.friends.find(friend => friend.id === friendId);
+      identifiedFriend.isFavorite = !identifiedFriend.isFavorite;
+    },
   },
 };
 </script>
