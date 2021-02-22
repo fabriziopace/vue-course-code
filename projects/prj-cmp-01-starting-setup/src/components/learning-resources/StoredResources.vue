def9d59d1530324a9b07fb4ea4ed5b39b62f5dc0
Fabrizio Pace <pacefabriziowork@gmail.com>
2021-02-22 21:56:05 +0100

    Deleting Items

--- projects/prj-cmp-01-starting-setup/src/components/learning-resources/StoredResources.vue
+++ projects/prj-cmp-01-starting-setup/src/components/learning-resources/StoredResources.vue
@@ -3,6 +3,7 @@
     <learning-resource
       v-for="res in resources"
       :key="res.id"
+      :id="res.id"
       :title="res.title"
       :description="res.description"
       :link="res.link"
@@ -16,8 +17,8 @@ import LearningResource from './LearningResource.vue';
 export default {
   inject: ['resources'],
   components: {
-    LearningResource
-  }
+    LearningResource,
+  },
 };
 </script>
 
